97d7af1dfec98772fadbe029a9120faa09891dd7
Arunav1 <93082604+Arunav1@users.noreply.github.com>
2023-11-28 06:27:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import LoginSighup from './Components/LoginSighup/LoginSighup'
+
+function App() {
+  return (
+    <div>
+      <LoginSighup />
+    </div>
+  );
+}
+
+export default App;
